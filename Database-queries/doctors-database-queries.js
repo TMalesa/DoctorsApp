bd4c5ed6b4f2f932e559adf9b302229f227108d5
Tshegofasto Malesa (ZA) <Tshegofasto.Malesa@absa.africa>
2022-05-10 12:32:47 +0200

    adding link patient to doctors function and link appointment to doctors function

--- Database-queries/doctors-database-queries.js
+++ Database-queries/doctors-database-queries.js
@@ -28,10 +28,34 @@ const createDoctor = async ({ name, surname, specialisation, location, fee, pati
         patientId: patientId,
         appointmentId: appointmentId
     });
+
     return doctors;
 }
 
-// const updateDoctor=async(doctorsId,{name, surname, specialisation, location, fee, patientId, appointmentId})=>{
-//     await DoctorsModel
-// }
-module.exports = { getAllDoctors, getDoctorsById, deleteDoctorsById, createDoctor }
+const updateDoctor = async (doctorsId, { name, surname, specialisation, location, fee, patientId, appointmentId }) => {
+    await DoctorsModel.updateOne({ _id: doctorsId },
+        {
+            name: name,
+            surname: surname,
+            specialisation: specialisation,
+            location: location,
+            fee: fee,
+            patientId: patientId,
+            appointmentId: appointmentId
+        });
+
+    return `Operation updated successfully `
+}
+
+const linkPatientToDoctor = async (doctorsId, patient) => {
+    await DoctorsModel.updateOne({ _id: doctorsId }, { patientId: patient });
+
+    return `Operation completed successfully `
+}
+
+const linkAppointmentToDoctor = async (doctorsId, appointment) => {
+    await DoctorsModel.updateOne({ _id: doctorsId }, { appointmentId: appointment });
+
+    return `Operation completed successfully `
+}
+module.exports = { getAllDoctors, getDoctorsById, deleteDoctorsById, createDoctor, updateDoctor, linkPatientToDoctor, linkAppointmentToDoctor }
